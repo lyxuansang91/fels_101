345cb1b0a17ed1513fa871ca42fd9cf38a1f43b0
Ly Xuan Sang <lyxuansang1991@gmail.com>
2016-01-18 15:52:06 +0700

    Add Activity

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,6 +6,7 @@ class Lesson < ActiveRecord::Base
   has_many :results, dependent: :destroy
   validate :check_number_of_words, on: :create
   accepts_nested_attributes_for :results
+  after_create :add_lesson_activity
 
   def number_correct_answer
     answers.where(correct_answer: true).count
@@ -17,5 +18,10 @@ class Lesson < ActiveRecord::Base
     if number_of_words < Settings.word.LESSON_SIZE
       errors.add :base, I18n.t("lesson.not_enough_word")
     end
+  end
+
+  def add_lesson_activity
+    activity_content = I18n.t "activity.add_lesson", user_name: user.name, category_name: category.name
+    Activity.create! user_id: user.id, activities: activity_content
   end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,13 @@ class Relationship < ActiveRecord::Base
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
+  after_create :add_follow_activity
+
+  private
+  def add_follow_activity
+    follow_activity_content = I18n.t "activity.add_follow_user", follower_user:
+      follower.name, followed_user: followed.name
+    Activity.create user_id: self.follower_id, activities:
+      follow_activity_content
+  end
 end
